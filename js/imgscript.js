480148f8448017d6622c381a2dcf7167f58b1a93
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-02 16:29:36 +0200

    BUG FIX

--- js/imgscript.js
+++ js/imgscript.js
@@ -1,22 +1,28 @@
 var bestHeight = "160px";
 
-function Resize33(picture)
+// Verkar inte funka att båda funktionerna anropas.
+// Och height verkar inte funka heller!
+// Vad gjorde egentligen flex för nytta?
+
+function Resize33(pic1)
 {
-    var p1 = document.getElementById(picture);
+    var p1 = document.getElementById(pic1);
 //    var x = p1.width;
     bestHeight = p1.style.height;
  //   p1.removeAttribute("height");
 //    p1.style.height = bestHeight;
 //    p1.removeAttribute("width");
 //    p1.style.width = "50px";
+    alert("Get height: " + bestHeight);
 
 }
 
-function Resize34(picture)
+function Resize34(pic2)
 {
-    var p2 = document.getElementById(picture);
-    p1.removeAttribute("height");
-    p1.style.height = bestHeight;
+    alert("BestHeight: " + bestHeight);
+    var p2 = document.getElementById(pic2);
+    p2.removeAttribute("height");
+    p2.style.height = bestHeight;
 //    p1.removeAttribute("width");
 //    p1.style.width = "50px";
 
