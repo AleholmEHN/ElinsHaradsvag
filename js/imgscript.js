d8cbc368bd1624ba61bb8383c8a36873d3d5024a
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-02 15:58:05 +0200

    nu funkar sidan, ej snygg skalning dock

---
+++ js/imgscript.js
@@ -0,0 +1,36 @@
+
+/*function Resize(picture)
+{
+    var p1 = document.getElementById(picture);
+    var x = p1.width;
+    var newH = x*132/200;
+    p1.height = newH;
+}*/
+
+/*
+
+function Resize33(pic1)
+{
+    var p1 = document.getElementById(pic1);
+//    alert("Old h: " + p1.height);
+    newH = p1.height;
+    //  p1.style.height = 500;
+    
+
+}
+
+function Resize34(pic2)
+{
+    var p2 = document.getElementById(pic2);
+/*    var x = p1.width;
+    newH = x*132/200; *//
+//    alert("kyrka h: " + p2.height + " new h: " + newH);
+ /*   p2.height = newH;
+}*/
+
+/*function Test()
+{
+    alert("Testar");
+} */
+
+
