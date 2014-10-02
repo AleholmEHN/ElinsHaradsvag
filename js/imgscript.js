dc9de436d63b323f65214080edc0e9d20f4c7c35
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-02 16:48:16 +0200

    best height test

--- js/imgscript.js
+++ js/imgscript.js
@@ -8,18 +8,18 @@ function Resize33(pic1)
 {
     var p1 = document.getElementById(pic1);
 //    var x = p1.width;
-//    bestHeight = p1.style.height;
+    bestHeight = p1.height + "px";
  //   p1.removeAttribute("height");
 //    p1.style.height = bestHeight;
 //    p1.removeAttribute("width");
 //    p1.style.width = "50px";
-    alert("Get height: " + bestHeight);
+ //   alert("Get height: " + bestHeight);
 
 }
 
 function Resize34(pic2)
 {
-    alert("BestHeight: " + bestHeight);
+//    alert("BestHeight: " + bestHeight);
     var p2 = document.getElementById(pic2);
     p2.removeAttribute("height");
     p2.style.height = bestHeight;
