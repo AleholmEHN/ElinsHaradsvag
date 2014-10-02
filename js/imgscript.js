b7ba81040f9691d5faecc4cd83c211996af9694c
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-02 16:21:10 +0200

    hgfhjg

---
+++ js/imgscript.js
@@ -0,0 +1,24 @@
+var bestHeight = "160px";
+
+function Resize33(picture)
+{
+    var p1 = document.getElementById(picture);
+//    var x = p1.width;
+    bestHeight = p1.style.height;
+ //   p1.removeAttribute("height");
+//    p1.style.height = bestHeight;
+//    p1.removeAttribute("width");
+//    p1.style.width = "50px";
+
+}
+
+function Resize34(picture)
+{
+    var p2 = document.getElementById(picture);
+    p1.removeAttribute("height");
+    p1.style.height = bestHeight;
+//    p1.removeAttribute("width");
+//    p1.style.width = "50px";
+
+}
+
