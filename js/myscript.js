d2c0b75c0f8f9f10d5fc15d269db82db39a7f176
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-06 21:06:06 +0200

    Tagit bort onödigt. Fixat brun färg

--- js/myscript.js
+++ js/myscript.js
@@ -12,36 +12,6 @@
         });
 
 
-/*var bestHeight = "100px";
-
-function GetSize(pic1)
-{
-    var p1 = document.getElementById(pic1);
-    bestHeight = p1.height + "px";
- 
-    p1.style.height = bestHeight;
-    // Bild height
-    var p3 = document.getElementById('bild1');
-    var bildHeight = p1.height + 6;     // Dubbel marginal, uppe och nere
-    bildHeight += "px";
-
-    p3.style.height = bildHeight;
-    p3 = document.getElementById('bild2');
-    p3.style.height = bildHeight;
-    p3 = document.getElementById('bild3');
-    p3.style.height = bildHeight;
-
-
-
-}
-
-function SetSize(pic2)
-{
-    var p2 = document.getElementById(pic2);
-    p2.style.height = bestHeight;
-     
-}*/
-
 function fixPicSize()
 {
     var p1 = document.getElementById('left_pic');
