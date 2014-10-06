3048a56d9fc9d4e25fd647685aa44964cc48def3
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-06 14:38:44 +0200

    testar med röd  bildkant för att se problemet

--- js/myscript.js
+++ js/myscript.js
@@ -21,12 +21,12 @@ function GetSize(pic1)
     
     
     // Försöker fixa vita kanten
-    var slider = document.getElementById(slider);
+/*    var slider = document.getElementById(slider);
     slider.style.height = "100px";
     var b1 = document.getElementById(bild1);
     bild1.style.height = "100px";
     bild1.style.marginBottom = 0;
-    bild1.style.paddingBottom = 0;
+    bild1.style.paddingBottom = 0;*/
 
 }
 
