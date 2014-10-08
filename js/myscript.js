b9555c32e538fcfdfb7f722051f853eaf8063b5c
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-08 18:40:11 +0200

    provar mindre bredd

--- js/myscript.js
+++ js/myscript.js
@@ -20,19 +20,27 @@ function fixPicSize()
     var p1 = document.getElementById('left_pic');
     var picHeight = p1.height + "px";
     var bildHeight = p1.height + 6+ "px";
+
     p1.style.height = picHeight;
+
+    p1 = document.getElementById('slider');
+    p1.style.height = picHeight;
+    
     p1 = document.getElementById('middle_pic');
     p1.style.height = picHeight;
+
+    
     p1 = document.getElementById('right_pic');
     p1.style.height = picHeight;
+    
     p1 = document.getElementById('bild1');
     p1.style.height = bildHeight;
+    
     p1 = document.getElementById('bild2');
     p1.style.height = bildHeight;
+    
     p1 = document.getElementById('bild3');
     p1.style.height = bildHeight;
-//    p1 = document.getElementById('slider');
-//    p1.style.height = picHeight;
     
 }
 
