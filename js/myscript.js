1dd45f0cc09c45e14b4edb530f827dab7a20a2f9
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-08 11:44:45 +0200

    Nu är kanten OK, men ej kartan

--- js/myscript.js
+++ js/myscript.js
@@ -28,6 +28,8 @@ function fixPicSize()
     p1.style.height = bildHeight;
     p1 = document.getElementById('bild3');
     p1.style.height = bildHeight;
+    p1 = document.getElementById('slider');
+    p1.style.height = picHeight;
     
 }
 
