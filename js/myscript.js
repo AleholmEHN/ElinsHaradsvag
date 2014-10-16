4c0a41cf23f5b69d4d4d74c0a4e1a620b28c95e6
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-16 12:52:44 +0200

    borders

--- js/myscript.js
+++ js/myscript.js
@@ -42,15 +42,17 @@ function fixPicSize()
     p1.style.height = bildHeight;
     p1.removeAttribute(p1.style.border.borderLeft);
     p1.removeAttribute(p1.style.border.borderRight);
-     p1.style.borderLeft = "10";
-    p1.style.borderRight = "10";
+    p1.style.borderLeft = "10 px";
+    p1.style.borderRight = "10 px";
  
     p1.style.borderColor = "red";
     
     p1 = document.getElementById('bild3');
     p1.style.height = bildHeight;
-    p1.style.borderLeft = "0px";
-    p1.style.borderRight = "0px";
+    p1.removeAttribute(p1.style.border.borderLeft);
+    p1.removeAttribute(p1.style.border.borderRight);
+    p1.style.borderLeft = "10px";
+    p1.style.borderRight = "10px";
     p1.style.borderColor = "blue";
     
     // Prova fixa Samsung problem i kod
