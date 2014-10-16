c616c1b698aa5661ea555667af9ead69b5e3819d
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-16 12:49:17 +0200

    tjocka borders

--- js/myscript.js
+++ js/myscript.js
@@ -40,13 +40,15 @@ function fixPicSize()
     
     p1 = document.getElementById('bild2');
     p1.style.height = bildHeight;
-    p1.style.borderLeft = "3px";
-    p1.style.borderRight = "3px";
+    p1.removeAttribute(p1.style.border);
+    p1.style.borderLeft = "10px";
+    p1.style.borderRight = "10px";
+    p1.style.border="5px";
     p1.style.borderColor = "red";
     
     p1 = document.getElementById('bild3');
     p1.style.height = bildHeight;
-    p1.style.borderLeft = "0 px";
+    p1.style.borderLeft = "0px";
     p1.style.borderRight = "0px";
     p1.style.borderColor = "blue";
     
