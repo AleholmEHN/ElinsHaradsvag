bae5055f6529f13640c92b4db66248b884523fd6
Elin Henriksson <elin.henriksson@savsjo.se>
2014-10-28 21:29:34 +0100

    Tester med navigering. Ej OK

--- erik.js
+++ erik.js
@@ -18,14 +18,36 @@
                         $menuLeft.toggleClass('pushmenu-open');
                 });
             
+            // Right menu
+                $menuLeft2 = $('.pushmenu2-left');
+                $nav_list2 = $('#nav_list2');
+                 
+                $nav_list2.click(function() {
+                        $(this).toggleClass('active');
+                        $('.pushmenu2-push').toggleClass('pushmenu2-push-toright');
+                        $menuLeft2.toggleClass('pushmenu2-open');
+                });
+
+             // Right menu
+                $menuLeft3 = $('.pushmenu3-left');
+                $nav_list3 = $('#nav_list3');
+                 
+                $nav_list3.click(function() {
+                        $(this).toggleClass('active');
+                        $('.pushmenu3-push').toggleClass('pushmenu3-push-toright');
+                        $menuLeft3.toggleClass('pushmenu3-open');
+                });
+
+           $('.slider').css("top", $("#header_image").height() + 42 + "px");
+   
+            $("#ruta1").css("top", $("#header_image").height() + $('.slider').height() +42+ "px");
+ 
+            
             $("#appkarta").css("height", ($(window).height() - 42) + "px");
-+		$("#appkarta").css("width", $(window).width() + "px");
+		$("#appkarta").css("width", $(window).width() + "px");
 
             
             
-            $('.slider').css("top", $("#header_image").height() + 42 + "px");
-   
-            $("#ruta1").css("top", $("#header_image").height() + $('.slider').height() +42+ "px");
             
         });
 
